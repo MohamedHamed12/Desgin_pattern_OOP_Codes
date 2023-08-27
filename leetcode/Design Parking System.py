ccbc0e4a8c3287fff32d12c75a5f7cd000e1228f
MohamedHamed12 <mh1779371@gmail.com>
2023-08-27 16:38:04 +0300

    Parking

--- leetcode/Design Parking System.py
+++ leetcode/Design Parking System.py
@@ -1,4 +1,35 @@
 
+# bad code
+class ParkingSystem:
+    def __init__(self, big: int, medium: int, small: int):
+        self.big = big
+        self.medium = medium
+        self.small = small
+    def addCar(self, carType: int) -> bool:
+        if carType == 1:
+            if self.big > 0:
+                self.big -= 1
+                return True
+            else:
+                return False
+        elif carType == 2:
+            if self.medium > 0:
+                self.medium -= 1
+                return True
+            else:
+                return False
+        elif carType == 3:
+            if self.small > 0:
+                self.small -= 1
+                return True
+            else:
+                return False
+a = ParkingSystem(1, 1, 0)
+print(a.addCar(1))
+
+
+
+
 # Utilizing Design Patterns:
 #   Factory Pattern , Composition Pattern
 # 
@@ -63,31 +94,3 @@ class ParkingSystem:
         self.spots[CarType.Big] = ParkingSpot(capacity)
 
 
-
-# bad code
-class ParkingSystem:
-    def __init__(self, big: int, medium: int, small: int):
-        self.big = big
-        self.medium = medium
-        self.small = small
-    def addCar(self, carType: int) -> bool:
-        if carType == 1:
-            if self.big > 0:
-                self.big -= 1
-                return True
-            else:
-                return False
-        elif carType == 2:
-            if self.medium > 0:
-                self.medium -= 1
-                return True
-            else:
-                return False
-        elif carType == 3:
-            if self.small > 0:
-                self.small -= 1
-                return True
-            else:
-                return False
-a = ParkingSystem(1, 1, 0)
-print(a.addCar(1))
